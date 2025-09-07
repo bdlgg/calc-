8cbd78603ac82696b40f5a674c52e4b047df1337
aleksejkonstantinov <40akka@gmail.com>
2025-09-07 16:00:50 +0300

    исправил добавил функцию памяти

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,120 +13,131 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             double one, two, result;
+            double memory = 0;
             string znak;
             Console.WriteLine("Добро пожаловать в калькулятор. Вам необходимо ввести первое\nчисло, затем знак действия(+,-,*,/, %, ^2, sqrt, 1/x, m+, m-, mr ), которое хотите совершить и второе число.");
-            Console.Write("Введите первое число");
-            one = Convert.ToSingle(Console.ReadLine());
-            Console.WriteLine("Введите знак действия");
-            znak = Convert.ToString(Console.ReadLine());
-            if (znak != "+" || znak != "-" || znak != "*" || znak != "/" || znak != "%" || znak != "^2" ||
-                znak != "sqrt" || znak != "1/x" || znak != "m+" || znak != "mr" || znak != "m-")
+            Console.WriteLine("Чтобы выйти напишите exit вместо знака");
+            while (true) // цикл пока пользователь не введет exit
             {
-                Console.WriteLine("неизвестный знак!");
-            }
-            else
-            { 
+                Console.Write("Введите первое число: ");
+                if (!double.TryParse(Console.ReadLine(), out one))
+                {
+                    Console.WriteLine("Ошибка ввода числа, попробуйте снова.\n");
+                    continue;
+                }
+
+                Console.Write("Введите знак действия: ");
+                znak = Console.ReadLine();
+
+                if (znak == "exit")
+                {
+                    Console.WriteLine("Выход из калькулятора.");
+                    break;
+                }
+
+                // проверка корректности знака
+                if (znak != "+" && znak != "-" && znak != "*" && znak != "/" && znak != "%" &&
+                    znak != "^2" && znak != "sqrt" && znak != "1/x" &&
+                    znak != "m+" && znak != "m-" && znak != "mr")
+                {
+                    Console.WriteLine("Неизвестный знак!\n");
+                    continue;
+                }
+
+                // арифметика
                 if (znak == "+")
                 {
-                    Console.WriteLine("Введите второе число");
-                    two = Convert.ToSingle(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    two = Convert.ToDouble(Console.ReadLine());
                     result = one + two;
-                    Console.WriteLine("результат: " +  result);
-                    Console.WriteLine("для выхода нажмите любую клавишу");
-                    Console.ReadKey();
+                    Console.WriteLine("Результат: " + result + "\n");
                 }
                 if (znak == "-")
                 {
-                    Console.WriteLine("Введите второе число");
-                    two = Convert.ToSingle(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    two = Convert.ToDouble(Console.ReadLine());
                     result = one - two;
-                    Console.WriteLine("результат: " +  result);
-                    Console.WriteLine("для выхода нажмите любую клавишу");
-                    Console.ReadKey();
+                    Console.WriteLine("Результат: " + result + "\n");
                 }
                 if (znak == "*")
                 {
-                    Console.WriteLine("Введите второе число");
-                    two = Convert.ToSingle(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    two = Convert.ToDouble(Console.ReadLine());
                     result = one * two;
-                    Console.WriteLine("результат: " +  result);
-                    Console.WriteLine("для выхода нажмите любую клавишу");
-                    Console.ReadKey();
+                    Console.WriteLine("Результат: " + result + "\n");
                 }
                 if (znak == "/")
                 {
-                    Console.WriteLine("Введите второе число");
-                    two = Convert.ToSingle(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    two = Convert.ToDouble(Console.ReadLine());
                     if (two == 0)
                     {
-                        Console.WriteLine("на 0 делить нельзя!");
-                        Console.WriteLine("для выхода нажмите любую клавишу");
-                        Console.ReadKey();
+                        Console.WriteLine("На 0 делить нельзя!\n");
                     }
                     else
                     {
                         result = one / two;
-                        Console.WriteLine("результат: " +  result);
-                        Console.WriteLine("для выхода нажмите любую клавишу");
-                        Console.ReadKey();
+                        Console.WriteLine("Результат: " + result + "\n");
                     }
                 }
                 if (znak == "%")
                 {
-                    Console.WriteLine("Введите второе число");
-                    two = Convert.ToSingle(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    two = Convert.ToDouble(Console.ReadLine());
                     if (two == 0)
                     {
-                        Console.WriteLine("на 0 делить нельзя!");
-                        Console.WriteLine("для выхода нажмите любую клавишу");
-                        Console.ReadKey();
+                        Console.WriteLine("На 0 делить нельзя!\n");
                     }
                     else
                     {
                         result = one % two;
-                        Console.WriteLine("результат: " +  result);
-                        Console.WriteLine("для выхода нажмите любую клавишу");
-                        Console.ReadKey();
+                        Console.WriteLine("Результат: " + result + "\n");
                     }
                 }
                 if (znak == "^2")
                 {
                     result = one * one;
-                    Console.WriteLine("результат: " +  result);
-                    Console.WriteLine("для выхода нажмите любую клавишу");
-                    Console.ReadKey();
+                    Console.WriteLine("Результат: " + result + "\n");
                 }
                 if (znak == "1/x")
                 {
                     if (one == 0)
                     {
-                        Console.WriteLine("на 0 делить нельзя!");
-                        Console.WriteLine("для выхода нажмите любую клавишу");
-                        Console.ReadKey();
+                        Console.WriteLine("На 0 делить нельзя!\n");
                     }
                     else
                     {
-                        result = 1/one;
-                        Console.WriteLine("результат: " +  result);
-                        Console.WriteLine("для выхода нажмите любую клавишу");
-                        Console.ReadKey();
+                        result = 1 / one;
+                        Console.WriteLine("Результат: " + result + "\n");
                     }
                 }
                 if (znak == "sqrt")
                 {
                     if (one < 0)
                     {
-                        Console.WriteLine("корень из отрицательного числа невозможен!");
-                        Console.WriteLine("для выхода нажмите любую клавишу");
-                        Console.ReadKey();
+                        Console.WriteLine("Корень из отрицательного числа невозможен!\n");
                     }
                     else
                     {
                         result = Math.Sqrt(one);
-                        Console.WriteLine("результат: " +  result);
-                        Console.WriteLine("для выхода нажмите любую клавишу");
-                        Console.ReadKey();
+                        Console.WriteLine("Результат: " + result + "\n");
                     }
+                }
+
+                // операции памяти
+                if (znak == "m+")
+                {
+                    memory += one;
+                    Console.WriteLine("Память: " + memory + "\n");
+                }
+                if (znak == "m-")
+                {
+                    memory -= one;
+                    Console.WriteLine("Память: " + memory + "\n");
+                }
+                if (znak == "mr")
+                {
+                    Console.WriteLine("Значение из памяти: " + memory + "\n");
                 }
             }
         }
